50c45b7cc96fadab774f3187657e9aa50eab194e
Kendall Sutton <kendallsutton@gmail.com>
2021-02-02 03:14:58 -0500

    wip

--- install.lua
+++ install.lua
@@ -1,35 +1,85 @@
--- Install
+-- Configuration
 local github_repo = "kspear/turtle-tools"
 local branch = "master"
-local manifest = {
-    "main.lua",
-    "inventory.lua",
-    "startup.lua"
+
+local dependencies = {
+    {
+        name="jsonlua",
+        repo="rxi/json.lua",
+        files={
+            "json.lua"
+        }
+    }
 }
 
-function download(filename)
-    local url = "http://raw.githubusercontent.com/"..github_repo.."/"..branch.."/"..filename 
+function spit(filename, data)
+    local file = fs.open(filename, "w")
+    file.write(data)
+    file.close()    
+    response.close()
+end
+
+function slurp(filename)
+    local file = fs.open(filename, "r")
+    local data = file.readAll()
+    file.close()
+    return data
+end
+
+function download(repo, branch, filename, prefix="")
+    local url = "http://raw.githubusercontent.com/"..repo.."/"..branch.."/"..filename 
     local response = http.get(url)
+    local filename = prefix..filename
     write(filename.." ... ")
     local data = response.readAll()
     if fs.exists(filename) then
         fs.delete(filename)
     end
-    local file = fs.open(filename, "w")
-    file.write(data)
-    file.close()    
-    response.close()
+    spit(filename, data)
     write("Done\n")
 end
 
+-- Bootstrap
+function dependency()
+    for i1=1,#dependencies do
+        local dependency = dependencies[i1]
+        print(dependency["name"])
+        local files = dependency["files"]
+        for i2=1,#dependency["files"] do
+            download(
+                dependency["repo"],
+                dependency["branch"],
+                files[i2]
+            )
+        end
+    end
+end
+
+-- Install
+-- local manifest = {
+--     "main.lua",
+--     "inventory.lua",
+--     "startup.lua"
+-- }
+
+function manifest()
+    print("[Downloading manifest]")
+    download(github_repo, branch, "manifest.json")
+    json = require("json")
+    local raw = slurp("manifest.json")
+    return json.decode(raw)
+end
+
 function install()
+    local manifest = manifest()
     print("[Installing files]")
     for f=1,#manifest do
-        download(manifest[f])
+        download(github_repo, branch, manifest[f])
     end
 end
 
 function main()
+    dependency()
     install()
     print("[Installation Complete]")
 end
